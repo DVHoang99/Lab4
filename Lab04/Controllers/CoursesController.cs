cb3fc358b277c1db18820e8b9f5239b02dc34838
Administrator <Administrator@PC193>
2020-06-02 11:19:58 +0700

    Add project files.

---
+++ Lab04/Controllers/CoursesController.cs
@@ -0,0 +1,53 @@
+﻿using Lab04.Models;
+using Lab04.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab04.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create (CourseViewModel ViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", ViewModel);
+            }
+
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = ViewModel.GetDateTime(),
+                CategoryId = ViewModel.Category,
+                Place = ViewModel.Place
+            };
+
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
